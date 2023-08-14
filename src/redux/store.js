5c68395145fa91c9cd01b3c53c9605f32787ac79
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2023-08-14 19:19:17 +0200

    updated_store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: 'contacts',
